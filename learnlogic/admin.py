7f05ccc3ef7e1cee137fbf266ba864dfa3a39caf
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2024-11-22 23:05:26 +0000

    #Title:add admin register

--- learnlogic/admin.py
+++ learnlogic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+#adicionei os models para serem visualizados no django admin
+admin.site.register(Aluno)
+admin.site.register(Instrutor)
+admin.site.register(Material)
+admin.site.register(Resumo)
