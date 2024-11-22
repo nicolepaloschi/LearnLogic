c512848382849e293fe99f4813fe93182d8e8827
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2024-11-22 23:14:02 +0000

    #Title: add models em adminpy

--- learnlogic/admin.py
+++ learnlogic/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import Aluno, Instrutor, Material, Resumo
 
 # Register your models here.
 #adicionei os models para serem visualizados no django admin
