5e22fd5e49c655c69ab823dd84bdc5b0d3acefcd
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2024-12-13 20:00:23 +0000

    #Title: Add .html file and update url

--- apirest/urls.py
+++ apirest/urls.py
@@ -20,6 +20,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/learnlogic/', include('learnlogic.urls')),
+    path('', include('learnlogic.urls')),
     path('auth/', obtain_auth_token, name='api_token_auth'), #autenticação com token
 ]
